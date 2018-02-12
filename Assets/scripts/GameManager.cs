752f21a6d624dad316d27b9bb3c073c151b2d018
Ishaan Verma <ishaanverma97@gmail.com>
2018-02-12 21:45:06 +0530

    Adding Files

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    public GameObject completeScoreUI;
+
+    public void completeLevel()
+    {
+        completeLevelUI.SetActive(true);
+        completeScoreUI.SetActive(false);
+    }
+
+    public void endGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+
+            //Restart Game
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 100f;
+    public float upwardsForce = 50f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("JUMP");
+            rb.AddForce(0, upwardsForce * Time.deltaTime, 0, ForceMode.Impulse);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().endGame();
+        }
+
+    }
+}
